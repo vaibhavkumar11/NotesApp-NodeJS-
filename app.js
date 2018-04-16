379a8baa76825c7f62e16d20af5bfcca655ce062
Vaibhav Kumar <vaibhavkumarsvs@gmail.com>
2018-04-16 20:32:47 +0530

    Refactored Code and added remove function

--- app.js
+++ app.js
@@ -11,11 +11,21 @@ console.log("Command: ", command);
 console.log("Yargs", argv);
 
 if (command === "add") {
-   notes.addNotes(argv.title, argv.body);
+  var note = notes.addNotes(argv.title, argv.body);
+  if (note) {
+    console.log("Note Created");
+    console.log("------------");
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log("Note tite taken");
+  }
 } else if (command === "list") {
   notes.getAll();
 } else if (command === "read") {
   notes.getNotes(argv.title);
 } else if (command === "remove") {
-  notes.removeNotes(argv.title);
+  var removedNote = notes.removeNotes(argv.title);
+  var message = removedNote ? "Note has been removed" : "Note not found";
+  console.log(message);
 } else console.log("Command not recognized");
--- notes.js
+++ notes.js
@@ -1,22 +1,32 @@
 console.log("Starting Notes.js");
 const fs = require("fs");
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync("notes-data.json");
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = notes => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
+
 var addNotes = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title,
     body
   };
 
-  try {
-    var notesString = fs.readFileSync("notes-data.json");
-    notes = JSON.parse(notesString);
-  } catch (e) {}
+  var duplicateNotes = notes.filter(note => note.title === title);
 
-  var duplicateNotes = notes.filter(title => note.title === title);
   if (duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
 };
 
@@ -29,7 +39,10 @@ var getNotes = title => {
 };
 
 var removeNotes = title => {
-  console.log("Note has been removed");
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter(note => note.title !== title);
+  saveNotes(filteredNotes);
+  return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
